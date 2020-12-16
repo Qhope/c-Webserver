7e03ad7f43eb7820bd3baeba748af65540dccfc2
Qhope <vuqua@DESKTOP-FSP8KRT>
2020-12-16 21:07:54 +0700

    update

--- webServer/Program.cs
+++ webServer/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace webServer
 {
@@ -21,51 +18,102 @@ namespace webServer
                 var s = server.Accept();
                 var bReceive = new byte[1024];
                 var sss = s.Receive(bReceive);
-                string sBuffer = Encoding.ASCII.GetString(bReceive);
+                string rBuffer = Encoding.ASCII.GetString(bReceive);
 
 
-                var iStartPos = sBuffer.IndexOf("HTTP", 1);
-                string sHttpVersion = sBuffer.Substring(iStartPos, 8);
+                var iStartPos = rBuffer.IndexOf("HTTP", 1);
+                string sHttpVersion = rBuffer.Substring(iStartPos, 8);
 
 
                 // Extract the Requested Type and Requested file/directory  
-                var sRequest = sBuffer.Substring(0, iStartPos - 1);
+                var sRequest = rBuffer.Substring(0, iStartPos - 1);
                 var response = "";
+                var header = "";
+                var encodeheader= Encoding.ASCII.GetBytes(header);
+                if (sRequest.StartsWith("GET"))
+                {
+                    if (sRequest.Contains("index.html"))
+                    {
+                        response = File.ReadAllText("index.html");
 
-  
-                    if (sRequest.StartsWith("GET"))
-                    {
-                        if (sRequest.Contains("index.html"))
-                        {
-                            response = File.ReadAllText("index.html");
-                            
-                        }
-                        else if (sRequest.Contains("4k.JPG"))
-                        {
-                            var reponse = File.ReadAllBytes("4k.JPG");
-                        s.Send(reponse, reponse.Length, SocketFlags.None);
-                        var statusCode = sHttpVersion + "301" + "\r\n";
-                        var status = Encoding.ASCII.GetBytes(statusCode);
-                        s.Send(status);
+                        var length= Encoding.ASCII.GetBytes(response);
+                        header = header + sHttpVersion + " 200 OK" + "\r\n";
+                        header = header + "Server: cx1193719-b\r\n";
+                        header = header + "Content-Type: " + "text/html" + "\r\n";
+                        header = header + "Accept-Ranges: bytes\r\n";
+                        header = header + "Content-Length: " + length.Length + "\r\n\r\n";
+                        encodeheader = Encoding.ASCII.GetBytes(header); 
+                        Console.WriteLine(header);
+
+                        var sendBuffer = Encoding.ASCII.GetBytes(response);
+                        s.Send(encodeheader);
+                        s.Send(sendBuffer);
                     }
+                    else if (sRequest.Contains("4k.JPG"))
+                    {
+                        var responseImage = File.ReadAllBytes("4k.JPG");
+                       
+                        var length = Encoding.ASCII.GetBytes(response);
+                        header = header + sHttpVersion + " 200 OK" + "\r\n";
+                        header = header + "Server: cx1193719-b\r\n";
+                        header = header + "Content-Type: " + "image/jpeg" + "\r\n";
+                        header = header + "Accept-Ranges: bytes\r\n";
+                        header = header + "Content-Length: " + responseImage.Length + "\r\n\r\n";
+                        encodeheader = Encoding.ASCII.GetBytes(header);
+                        Console.WriteLine(header);
+
+                        s.Send(encodeheader);
+                        s.Send(responseImage, responseImage.Length, SocketFlags.None);
+                    }
+                    else if (sRequest.Contains("favicon.ico"))
+                    {
+                        var responseImage = File.ReadAllBytes("favicon.ico");
+
+                        header = header + sHttpVersion + " 200 OK" + "\r\n";
+                        header = header + "Server: cx1193719-b\r\n";
+                        header = header + "Content-Type: " + "image/x-icon" + "\r\n";
+                        header = header + "Accept-Ranges: bytes\r\n";
+                        header = header + "Content-Length: " + responseImage.Length + "\r\n\r\n";
+                        encodeheader = Encoding.ASCII.GetBytes(header);
+                        Console.WriteLine(header);
+
+                        var sendBuffer = Encoding.ASCII.GetBytes(response);
+                        s.Send(encodeheader);
+                        s.Send(responseImage, responseImage.Length, SocketFlags.None);
+                    }
+                    else if (sRequest.Contains("style.css"))
+                    {
+                       
+                        response = File.ReadAllText("style.css");
+
+                        var sendCss = Encoding.ASCII.GetBytes(response);
                         
+                        var length = Encoding.ASCII.GetBytes(response);
+                        header = header + sHttpVersion + " 200 OK" + "\r\n";
+                        header = header + "Server: cx1193719-b\r\n";
+                        header = header + "Content-Type: " + "text/css" + "\r\n";
+                        header = header + "Accept-Ranges: bytes\r\n";
+                        header = header + "Content-Length: " + response.Length + "\r\n\r\n";
+                        encodeheader = Encoding.ASCII.GetBytes(header);
+                        Console.WriteLine(header);
+
+                        var sendBuffer = Encoding.ASCII.GetBytes(response);
+                        s.Send(encodeheader);
+                        s.Send(sendCss);
                     }
-                    else if (sRequest.StartsWith("POST"))
+                    
+
+                }
+                else if (sRequest.StartsWith("POST"))
+                {
+                    var admin = "admin";
+                    if (admin == "admin")
                     {
-                        var admin = "admin";
-                        if (admin == "admin")
-                        {
-                            response = sHttpVersion + " 301 Moved Permanently \r\n l ocation: /info.html";
-                        }
+                        response = sHttpVersion + " 301 Moved Permanently \r\n location: /info.html";
+                       
                     }
-                Console.WriteLine(sBuffer);
-                //Console.WriteLine("sHttpVersion "  +sHttpVersion);
-                //Console.WriteLine("sRequest " + sRequest);
-                //var response = "400 hop";
-                ////response = sHttpVersion + " 404 Not Found" + "\r\n";
-                //response = sHttpVersion + " 200" + "\r\n";
-                var sendBuffer = Encoding.ASCII.GetBytes(response);
-                s.Send(sendBuffer);
+                }
+                Console.WriteLine(rBuffer);
                 s.Close();
             }
         }
